e6f45715f582a4ecf2e1237ebaf789656724f3ba
Borkuti Péter <peterborkuti@kuka-robotics.hu>
2018-10-15 08:40:27 +0200

    try

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/KUKAIisyMotion.java
@@ -0,0 +1,40 @@
+package hu.bp;
+
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.Form;
+
+@DesignerComponent(version = 1,
+        description = "Non-visible component that provides functions KUKA Iisy robot.",
+        category = ComponentCategory.EXTENSION,
+        nonVisible = true,
+        iconName = "", //"images/iisy.png",
+        showOnPalette = true)
+@SimpleObject(external = true)
+@UsesPermissions(permissionNames = "android.permission.INTERNET," +
+        "android.permission.WRITE_EXTERNAL_STORAGE," +
+        "android.permission.READ_EXTERNAL_STORAGE")
+@UsesLibraries(libraries = "json.jar")
+public class KUKAIisyMotion extends AndroidNonvisibleComponent {
+    private String robotName;
+
+    /*
+    @SimpleProperty(
+            description = "The name of the robot",
+            userVisible = true)
+    public String RobotName() {
+        return robotName;
+    }
+
+    @SimpleProperty(
+            userVisible = true)
+    public void RobotName(String robotName) {
+        this.robotName = robotName;
+    }
+    */
+
+    public KUKAIisyMotion(Form form) {
+        super(form);
+    }
+}
